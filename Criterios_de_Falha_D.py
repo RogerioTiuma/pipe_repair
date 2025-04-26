e346faf8bd209d355466f917d9dad8b297ddc7a2
RogerioTiuma <rogeriotiuma@hotmail.com>
2025-04-26 20:10:58 -0300

    Atualizando

---
+++ Criterios_de_Falha_D.py
@@ -0,0 +1,161 @@
+import math
+
+class CorrodedPipeline:
+    def __init__(self, D, t, d, l, w, ry, ru):
+        self.D = D
+        self.t = t
+        self.d = d
+        self.l = l
+        self.w = w
+        self.ry = ry
+        self.ru = ru
+
+    def stephens_leis(self):
+
+        M = 1 - math.exp(-0.157 * self.l / math.sqrt(self.D * (self.t - self.d) / 2))
+        Pb = (2 * self.t / self.D) * self.ru * (1 - M * (self.d / self.t))
+        return Pb
+
+    def choi(self):
+
+        Rt = math.sqrt(self.D / 2 * self.t)
+        
+        if self.l / Rt < 6:
+
+            C2 = 0.1163 * (self.d / self.t)**2 - 0.1053 * (self.d / self.t) + 0.0292
+            C1 = -0.6913 * (self.d / self.t)**2 + 0.4548 * (self.d / self.t) - 0.1447
+            C0 = 0.06 * (self.d / self.t)**2 - 0.1035 * (self.d / self.t) + 1.0
+            Pb = 0.9 * (2 * self.t / self.D) * self.ru * (C2 * (self.l / Rt)**2 + C1 * (self.l / Rt) + C0)
+
+        else:
+
+            C1 = 0.0071 * (self.d / self.t) - 0.0126
+            C0 = -0.9847 * (self.d / self.t) + 1.1101
+            Pb = (2 * self.t / self.D) * self.ru * (C1 * (self.l / Rt) + C0)
+
+        return Pb
+
+    def netto(self):
+
+        Pb = (2 * self.t / self.D) * self.ru * (1 - 0.9435 * (self.d / self.t)**1.6 * (self.l / self.D)**0.4)
+
+        return Pb
+
+    def ma(self):
+
+        n = 1 / (0.3 * (1 - (self.ry / self.ru)))
+
+        H = 2 / (math.sqrt(3) ** ((n + 1) / n))
+
+        M = 1 - 0.7501 * math.exp(-0.4174 * (self.l / math.sqrt(self.D * self.t))) * (1 - (self.d / self.t))**-0.01151
+        Pb = H * (2 * self.t / self.D) * self.ru * (1 - M * (self.d / self.t))
+
+        return Pb
+
+    def wang_zarghamee(self):
+
+        if self.D < 610:
+            Pb = (2 * self.t / self.D) * self.ru * (1 - 0.886 * (self.d / self.t)**1.0 * (self.l / self.D)**0.3)
+        else:
+            Pb = (2 * self.t / self.D) * self.ru * (1 - 1.120 * (self.d / self.t)**1.15 * (self.l / self.D)**0.3)
+
+        return Pb
+
+    def chen(self):
+
+        ratio_w_D = self.w / (math.pi * self.D)
+        ratio_l_Dt = self.l / math.sqrt(self.D * self.t)
+
+        if ratio_w_D <= 0.3:
+            if ratio_l_Dt <= 5:
+                C0 = 0.000194 + 0.0135 * (self.d / self.t) + 0.0221 * (self.d / self.t)**2
+                C1 = 0.00482 - 0.202 * (self.d / self.t) - 0.169 * (self.d / self.t)**2
+                C2 = 1.0604 - 0.253 * (self.d / self.t) + 0.194 * (self.d / self.t)**2
+                Pb = (2 * self.t / (self.D - self.t)) * self.ru * (C0 * ratio_l_Dt**2 + C1 * ratio_l_Dt + C2) * (C3 * ratio_w_D**2 + C4 * ratio_w_D + C5)
+            else:
+                C1 = 0.000238 - 0.0105 * (self.d / self.t)
+                C2 = 1.108 - 0.974 * (self.d / self.t)
+                C3 = -4.016 + 13.195 * (self.d / self.t)
+                C4 = 1.583 - 5.337 * (self.d / self.t)
+                C5 = 0.975 + 0.00873 * (self.d / self.t)
+                Pb = (2 * self.t / (self.D - self.t)) * self.ru * (C1 * ratio_l_Dt + C2) * (C3 * ratio_w_D**2 + C4 * ratio_w_D + C5)  
+        else:
+            if ratio_l_Dt <= 5:
+                C0 = -0.00239 + 0.0308 * (self.d / self.t) - 0.00382 * (self.d / self.t)**2
+                C1 = 0.0314 - 0.381 * (self.d / self.t) + 0.101 * (self.d / self.t)**2
+                C2 = 0.993 + 0.185 * (self.d / self.t) - 0.579 * (self.d / self.t)**2
+                Pb = (2 * self.t / (self.D - self.t)) * self.ru * (C0 * ratio_l_Dt**2 + C1 * ratio_l_Dt + C2)
+            else:
+                C1 = -0.000586 - 0.00771 * (self.d / self.t)
+                C2 = 1.129 - 1.0808 * (self.d / self.t)
+                Pb = (2 * self.t / (self.D - self.t)) * self.ru * (C1 * ratio_l_Dt + C2)
+        return Pb
+
+    def zhu(self):
+
+        n = 0.0763108
+
+        H = ((2 + math.sqrt(3)) / (4 * math.sqrt(3))) * (n + 1) 
+
+        ratio_l_Dt = self.l / math.sqrt(self.D * self.t)
+
+        M = 1 - (1 / (1 + 0.1385 * ratio_l_Dt + 0.1357 * ratio_l_Dt**2))
+
+        Pb = H * (4 * self.t / self.D) * self.ru * (1 - M * (self.d / self.t))
+
+        return Pb
+
+    def su(self):
+
+        if self.l < math.sqrt(20*self.D*self.t):
+
+            ratio_l_Dt = self.l / math.sqrt(self.D * self.t)
+            ratio_b_p = self.w / math.pi  # b é representado como w aqui
+
+            C0 = 0.8816 + 0.7942 * (self.d / self.t) - 0.05329 * (self.d / self.t)**2
+            C1 = 0.03982 - 0.3946 * (self.d / self.t) - 0.1901 * (self.d / self.t)**2
+            C2 = -0.004248 + 0.02983 * (self.d / self.t) + 0.03091 * (self.d / self.t)**2
+
+            G0 = 1.065 - 0.2992 * (self.d / self.t) - 0.248 * (self.d / self.t)**2
+            G1 = 0.06604 + 0.7039 * (self.d / self.t) - 2.027 * (self.d / self.t)**2
+            G2 = -0.000185 - 1.211 * (self.d / self.t) + 2.356 * (self.d  / self.t)**2
+
+            Pb = (2 * self.t / (self.D - self.t)) * self.ru * (C2 * ratio_l_Dt**2 + C1 * ratio_l_Dt + C0) *(G2 * ratio_b_p**2 + G1 * ratio_b_p + G0)
+        else:
+
+            Pb = 0
+
+        return Pb
+
+    def belachew(self):
+
+        l_D = self.l / self.D
+        if l_D <= 1:
+            Pb = (2 * self.t / (self.D - self.t)) * self.ru * (1 - 0.8 * (self.d / self.t) * (l_D)**0.4)
+        elif 1 < l_D < 2:
+            Pb = (2 * self.t / (self.D - self.t)) * self.ru * (1 - 0.8 * (self.d / self.t) * (l_D)**0.1)
+        else:
+            Pb = (2 * self.t / (self.D - self.t)) * self.ru * (1 - 0.85 * (self.d / self.t))
+
+        return Pb
+
+    def ghani(self):
+
+        n = 0.239 * (self.ry / self.ru - 1)**-0.596
+        H = 2 / 2**(n + 1) / n
+        M = 1 - 1 / (1 + (self.d / self.t) * (self.l / math.sqrt(self.D * self.t))**2)
+        Pb = H * (2 * self.t / (self.D - 2 * self.t)) * self.ru * (1 - M * (self.d / self.t))
+
+        return Pb
+
+    def phan(self):
+        P0 = (2 * self.t * self.ru) / (self.D - self.t)
+        Pb = P0 * (1 - 0.88555 * (self.d / self.t)**0.98077 * (self.l / self.D)**0.31053)
+        return Pb
+
+    def shuai(self):
+        ratio_w_D = self.w / (math.pi * self.D)
+        ratio_l_Dt = self.l / math.sqrt(self.D * self.t)
+        M = (1 - 0.1075 * (1 - ratio_w_D**2)**6 + 0.8925 * math.exp(-0.4103 * ratio_l_Dt**2)) * (1 - self.d / self.t)**0.2504
+        Pb = (2 * self.t / self.D) * self.ru * (1 - M * (self.d / self.t))
+        return Pb
--- main.py
+++ main.py
@@ -2,22 +2,50 @@ import asmepcc2 as pcc2
 
 import Material
 
-import Criterios_de_Falha as CF
+import Criterios_de_Falha_DR as CFDR
 
-P = 100.
-D = 50.
-E_c = 1.
-S = 1.
-t_s = 1.
-P_live = 0. # Pressão de operação do duto no momento do reparo
-E_s = 1.
-epsl_c = 1.
+from Criterios_de_Falha_D import CorrodedPipeline
+
+################ Duto #############
+
+D = 458.8        # Diâmetro Externo
+t = 41.9         # Espessura do Defeito
+
+################ Material do Duto ##############
+
+E_s = 1.         # Módulo de Elásticidade do Aço
+ry = 1.           # Tensão de Escoamento
+ru = 599.3       # Tensão de Última de Escoamento
+
+########## Defeito #########
+
+d = 4.9          # Profundidade do Defeito
+w = 132.0        # Largura do Defeito
+l = 771.6        # Comprimento do Defeito
+t_s = t-d        # t-d
+
+########## Geometria do Reparo ##########
+
+########## Material do Reparo ###########
+
+E_c = 1          # Módulo de Elásticidade do Compósito
+epsilon_c = 1.   # 
+
+
+########## Condições de Contorno #######
+
 P_i = 100
+P_live = 0. # Pressão de operação do duto no momento do reparo
+P = 100.
+
+
+epsl_c = 1.
+
 L = 10
 d = 5
 
 
-t = pcc2.repair_thickness(P, D, E_c, S, t_s, P_live,E_s,epsl_c).t_min()
+t = pcc2.repair_thickness(P, D, E_c, ry, t_s, P_live,E_s,epsl_c).t_min()
 
 #print(t.t_min())
 #print(E_s)
@@ -34,8 +62,26 @@ aco_1020 = Material.taterial(nome="Aço 1020",
 print(aco_1020)
 print(aco_1020.elasticidade)
 
-B31G = CF.fail.B31G(D,L,t,d,aco_1020.escoamento, aco_1020.elasticidade)
+B31G = CFDR.fail.B31G(D,L,t,d,aco_1020.escoamento, aco_1020.elasticidade)
 print(B31G)
 
-RSTRENG085 = CF.fail.RSTRENG085(D,L,t,d,aco_1020.escoamento, aco_1020.elasticidade)
+RSTRENG085 = CFDR.fail.RSTRENG085(D,L,t,d,aco_1020.escoamento, aco_1020.elasticidade)
 print(RSTRENG085[1])
+
+fail = CFDR.fail(D,L,t,P_i,d,aco_1020.escoamento, aco_1020.elasticidade)
+print()
+
+pipe = CorrodedPipeline(D, t, d, l, w, ry, ru)
+
+print(f"Pressão de falha (Stephens & Leis): {pipe.stephens_leis():.2f} MPa")
+print(f"Pressão de falha (Choi et al.): {pipe.choi():.2f} MPa")
+print(f"Pressão de falha (Netto et al.): {pipe.netto():.2f} MPa")
+print(f"Pressão de falha (Ma et al.): {pipe.ma():.2f} MPa")
+print(f"Pressão de falha (Wang & Zarghamee): {pipe.wang_zarghamee():.2f} MPa")
+print(f"Pressão de falha (Chen et al.): {pipe.chen():.2f} MPa")
+print(f"Pressão de falha (Zhu): {pipe.zhu():.2f} MPa")
+print(f"Pressão de falha (Su et al.): {pipe.su():.2f} MPa")
+print(f"Pressão de falha (Belachew et al.): {pipe.belachew():.2f} MPa")
+print(f"Pressão de falha (Ghani et al.): {pipe.ghani():.2f} MPa")
+print(f"Pressão de falha (Phan et al.): {pipe.phan():.2f} MPa")
+print(f"Pressão de falha (Shuai et al.): {pipe.shuai():.2f} MPa")
